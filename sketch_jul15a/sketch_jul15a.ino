645dc41f07841daa1c8ae77ba6986947905764e2
yashweblife <yashweblife20@gmail.com>
2021-07-16 19:00:25 -0500

    Updated Code

--- sketch_jul15a/sketch_jul15a.ino
+++ sketch_jul15a/sketch_jul15a.ino
@@ -1,27 +1,36 @@
 #include <ESP8266WiFi.h>
 
-String  i;
-String wifi_name = "RIEMENSCHNEIDER";
-String wifi_pass = "2rxkv8mj4h";
-IPAddress staticIP306_10(192,168,1,10);
-IPAddress gateway306_10(192,168,1,1);
-IPAddress subnet306_10(255,255,255,0);
+String wifi_name = "Your Wifi";
+String wifi_pass = "Your Wifi Password";
+
+
+IPAddress local_IP(192, 168, 1, 184);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8);   //optional
+IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
 WiFiServer server(80);
 
+String  i;
 void setup()
 {
   i = "";
-Serial.begin(9600);
-pinMode(2, OUTPUT);
-  WiFi.disconnect();
-   WiFi.begin(wifi_name,wifi_pass);
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  //WiFi.disconnect();
+  WiFi.begin(wifi_name,wifi_pass);
+  /*
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+    Serial.println("STA Failed to configure");
+  }  
+  */
   while ((!(WiFi.status() == WL_CONNECTED))){
     Serial.print(".");
     delay(300);
   }
   Serial.println("Connected");
-  //WiFi.config(staticIP306_10, gateway306_10, subnet306_10);
+  
   Serial.println((WiFi.localIP().toString()));
   server.begin();
 }
@@ -40,24 +49,24 @@ void loop()
     Serial.println("Request");
     if (i.indexOf("LED=ON")>-1) {
       digitalWrite(2,LOW);
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-Type: text/html");
-      client.println("");
-      client.println("<!DOCTYPE HTML>");
-      client.println("<html>");
-      client.println("On");
-      client.println("</html>");
+      //client.println("HTTP/1.1 200 OK");
+      //client.println("Content-Type: text/html");
+      //client.println("");
+      //client.println("<!DOCTYPE HTML>");
+      //client.println("<html>");
+      //client.println("On");
+      //client.println("</html>");
       Serial.println(i);
       delay(1);
     } else if (i.indexOf("LED=OFF") >-1) {
       digitalWrite(2,HIGH);
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-Type: text/html");
-      client.println("");
-      client.println("<!DOCTYPE HTML>");
-      client.println("<html>");
-      client.println("Off");
-      client.println("</html>");
+      //client.println("HTTP/1.1 200 OK");
+      //client.println("Content-Type: text/html");
+      //client.println("");
+      //client.println("<!DOCTYPE HTML>");
+      //client.println("<html>");
+      //client.println("Off");
+      //client.println("</html>");
       Serial.println(i);
       delay(1);
     }
@@ -67,8 +76,10 @@ void loop()
     client.println("<!DOCTYPE HTML>");
     client.println("<html>");
     client.println("<head>");
+    client.println("<style>*{font-size:2em;margin:0;box-sizing:border-box;}body{width:100vw;height:100vh;display:flex;flex-direction:column;align-items:center;}button{padding:1em;width:100%;height:90%;display:flex;align-items:center;justify-content:center;}</style>");
     client.println("</head>");
     client.println("<body>");
+    client.println("<br>");
       client.println("<a href=");
       client.println("LED=ON");
       client.println("><button>");
